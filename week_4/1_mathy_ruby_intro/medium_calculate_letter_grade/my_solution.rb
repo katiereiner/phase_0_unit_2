70f9a903e95f47540c88cc5f37ac652d7684b598
Katie <kreiner@umich.edu>
2014-04-30 18:25:52 -0400

    medium calculate letter grade final solutions!

--- week_4/1_mathy_ruby_intro/medium_calculate_letter_grade/my_solution.rb
+++ week_4/1_mathy_ruby_intro/medium_calculate_letter_grade/my_solution.rb
@@ -6,17 +6,33 @@
 
 # I worked on this challenge [by myself, with: ].
 
-# 1. Pseudocode
+# 1. Pseudocode 
+=begin 
+count = 0
+sum = 0
+for i in my array
+	sum = i
+	count ++,
+	end
+	puts sum/count
+=end
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
+# What is the input? input is number grades 
+# What is the output? (i.e. What should the code return?) 
+#output is letter grade
 # What are the steps needed to solve the problem?
 
 
 # 2. Initial Solution
-
-
-
+def avg(my_array)
+	var="sum"
+	sum = 0
+	for i in my_array
+		sum += i 
+	end
+		puts sum/my_array.length
+end
+avg([88,96,54,67,86,35,98,100])
 
 # 3. Refactored Solution
 
