9d93690f9ec859f4410e7bb32e942cd32e3f9d45
Katie <kreiner@umich.edu>
2014-04-30 17:57:08 -0400

    final easy_add_it_up solutions!

--- week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
+++ week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
@@ -4,9 +4,17 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 1. Pseudocode
+
+=begin
+define total (my_array)
+sum = 0
+for x in my array
+	sum t = x
+	print "total" = sum
+=end
 
 # What is the input?
 # What is the output? (i.e. What should the code return?)
@@ -14,10 +22,31 @@
 
 
 # 2. Initial Solution
+def total(my_array)
+	var="sum"
+	sum = 0
+	for i in my_array
+		sum += i 
+	end
+		puts sum
+end
 total([1,2,3,4])
+# => 1
+# => 3
+# => 6
 # => 10
 
-sentence_maker (["i", "want", "to", "learn", "how", "to", "code"])
+def sentence_maker(my_array)
+	var="sentence"
+	sentence = ""
+	for i in my_array
+		sentence += i
+		sentence += " "
+	end
+		sentence += "."
+		puts sentence
+end
+sentence_maker(["i", "want", "to", "learn", "how", "to", "code"])
 # => "I want to learn how to code"
 
 
