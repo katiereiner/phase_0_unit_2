311d79b6e12b77bd1cbfc6689a63977aa7c3c20f
Katie <kreiner@umich.edu>
2014-05-01 16:36:45 -0400

    Final Creative essay writer solutions!

--- week_4/2_creative/make_a_homework_cheater/my_solution.rb
+++ week_4/2_creative/make_a_homework_cheater/my_solution.rb
@@ -1,18 +1,22 @@
 # U2.W4: Homework Cheater!
 
 
-# I worked on this challenge [by myself, with: ].
-
+# I worked on this challenge [by myself]
 # 2. Pseudocode
 
-# Input:
-# Output:
-# Steps:
+# Input: Name, Title, Topic, Date, Thesis, Pronoun
+# Output: Katie, 
+# Steps: 
 
 
 # 3. Initial Solution
+def essay_writer(name, title, topic, date, thesis_statement, pronoun)
+  puts "This is an essay about #{name}. 
+  The title of this essay about #{name} is #{title}. #{name} was
+  very important because #{pronoun} #{thesis_statement} in #{date}."
+end
 
-
+essay_writer("A","B","C","D","E","F")
 
 
 # 4. Refactored Solution
