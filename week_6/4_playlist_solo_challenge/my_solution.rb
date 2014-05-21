d79399845d0bc1de7d68c744400c059fd9acdbac
Katie <kreiner@umich.edu>
2014-05-21 15:16:30 -0400

    Solo Challenge complete!

--- week_6/4_playlist_solo_challenge/my_solution.rb
+++ week_6/4_playlist_solo_challenge/my_solution.rb
@@ -26,6 +26,10 @@ class Playlist
 		@song.push(*new_song)
 	end
 
+	def num_of_tracks
+		@song.count
+	end
+
 end
 
 
@@ -50,12 +54,12 @@ lying_from_you = Song.new("Lying From You", "Linkin Park")
 angels = Song.new("Angels", "Within Temptation")
  
 my_playlist.add(lying_from_you, angels)
-#p my_playlist.num_of_tracks == 5
-#going_under.play
-#my_playlist.remove(angels)
-#p my_playlist.includes?(lying_from_you) == true
-#my_playlist.play_all
-#my_playlist.display
+p my_playlist.num_of_tracks == 5
+going_under.play
+my_playlist.remove(angels)
+p my_playlist.includes?(lying_from_you) == true
+my_playlist.play_all
+my_playlist.display
 
 print my_playlist.song
 
